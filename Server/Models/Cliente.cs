e911a4eed99da54e3f554133a286af06d1bf5d3e
Johan Ullola <johanullola@gmail.com>
2023-03-20 20:06:30 -0400

    error

--- Server/Models/Cliente.cs
+++ Server/Models/Cliente.cs
@@ -1,11 +1,15 @@
+using System.Net.Cache;
+using CellSystem.Shared.Requests;
+using CellSystem.Shared.Records;
 using System.ComponentModel.DataAnnotations;
 
 namespace CellSystem.Server.Models;
 
-public class Clientes
+public class Cliente
 {
            [Key]
         public string Nombre {get; set;}= null!;
-        public int cedula{get; set;} 
+        public int Cedula{get; set;} 
         public int Telefono {get; set;}
+
 }
--- Shared/Requests/ClienteCreateRequest.cs
+++ Shared/Requests/ClienteCreateRequest.cs
@@ -1,16 +1,15 @@
+
 using System.ComponentModel.DataAnnotations;
 
 namespace CellSystem.Shared.Requests;
 
-public class ClienteUpdateRequest
+public class ClienteCreateRequest
 {   
         [Required (ErrorMessage="Proporcione un Nombre")]
         public string Nombre {get; set;}= null!;
+
         public int cedula{get; set;} 
+
         public int Telefono {get; set;}  
 }
- public class ClienteCreateRequest: ClienteUpdateRequest
- {  
-        [Required (ErrorMessage ="Debe Proporcionar un nombre")]
-        public new string  Nombre { get; set; }=null!;
- }
+ 
