664ccff46f82130412891813ceb9338dca6f4047
Ronald Bottier <2019-0879@uteco.edu.do>
2023-03-20 20:13:24 -0400

    cambios

--- Server/Models/Cliente.cs
+++ Server/Models/Cliente.cs
@@ -1,6 +1,3 @@
-using System.Net.Cache;
-using CellSystem.Shared.Requests;
-using CellSystem.Shared.Records;
 using System.ComponentModel.DataAnnotations;
 using CellSystem.Shared.Records;
 using CellSystem.Shared.Requests;
@@ -13,7 +10,6 @@ public class Cliente
         public string Nombre {get; set;}= null!;
         public int Cedula{get; set;} 
         public int Telefono {get; set;}
-<<<<<<< HEAD
         public static Cliente Crear (ClienteCreateRequest request)
         {
                 return new Cliente(){
@@ -23,7 +19,4 @@ public class Cliente
 
                 };
         }
-=======
-
->>>>>>> e911a4eed99da54e3f554133a286af06d1bf5d3e
 }
